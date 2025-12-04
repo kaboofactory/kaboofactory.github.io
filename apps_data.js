40454c491614f6702c400a63ef7b4b54bc33538d
Kaboofactory <kaboofactory@gmail.com>
2025-12-05 00:22:31 +0900

    Added KabooEdgeExtractionTestImageMaker
    Added KabooEdgeExtractionTestImageMaker

--- apps_data.js
+++ apps_data.js
@@ -6,6 +6,12 @@ window.APP_LIST = [
     "icon": "./KabooColorReductionTool/application.png"
   },
   {
+    "name": "KabooEdgeExtractionTestImageMaker",
+    "path": "./KabooEdgeExtractionTestImageMaker/index.html",
+    "description": "エッジ抽出テスト用イメージ作成ツール",
+    "icon": "./KabooEdgeExtractionTestImageMaker/application.png"
+  },
+  {
     "name": "KabooEXIFChecker",
     "path": "./KabooEXIFChecker/index.html",
     "description": "EXIF確認・削除ツール",
